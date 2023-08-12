c8ab11bd7f5239c3418370437fa95394d686d36d
Carlos Magno <81724679+carlosmagno@users.noreply.github.com>
2023-08-11 22:39:19 -0300

    Update meuPrimeiroApp.dart

--- meuPrimeiroApp.dart
+++ meuPrimeiroApp.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MyApp(
    home:Scaffold(
-     
+     appBar: AppBar(
+       title: Text('titulo'),
+     ), 
+     body: Center(
+      child: Text('texto centralizado'),
+     ),
    )
   ));
   
